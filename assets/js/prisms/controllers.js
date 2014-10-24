859daa74c5fd8df1b1dfd3baf81cc848ab567679
Chia Hui Hsieh <chiahuih@umich.edu>
2014-10-24 11:06:23 -0400

    set the routes of navbar using mg-view

---
+++ assets/js/prisms/controllers.js
@@ -0,0 +1,29 @@
+'use strict';
+
+function FrontPageController($scope) {
+
+}
+
+function AboutController($scope) {
+
+}
+
+function NewsController($scope) {
+
+}
+
+function MaterialsCommonsController($scope) {
+
+}
+
+function PeopleController($scope) {
+
+}
+
+function PublicationsController($scope) {
+
+}
+
+function LinksController($scope) {
+
+}
---
+++ assets/js/prisms/prisms-app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var app = angular.module('prisms', []);
+
+app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+    $routeProvider.
+        when('/prisms', {templateUrl:'partials/front-page.html', controller:FrontPageController}).
+        when('/prisms/about', {templateUrl:'partials/about.html'}).
+        when('/prisms/people', {templateUrl:'partials/people.html', controller:PeopleController}).
+        when('/prisms/publications', {templateUrl:'partials/publications.html', controller:PublicationsController}).
+        otherwise({redirectTo:'/prisms'});
+}]);
+
