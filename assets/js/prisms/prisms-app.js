6abe0aed454fc9a8fd6388a0d840f8dfe71766a2
irissonghy <huaying@umich.edu>
2014-10-27 13:43:20 -0400

    about controller

--- assets/js/prisms/prisms-app.js
+++ assets/js/prisms/prisms-app.js
@@ -5,7 +5,7 @@ var app = angular.module('prisms', []);
 app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider.
         when('/prisms', {templateUrl:'partials/front-page.html', controller:FrontPageController}).
-        when('/prisms/about', {templateUrl:'partials/about.html'}).
+        when('/prisms/about', {templateUrl:'partials/about.html', controller:AboutController}).
         when('/prisms/people', {templateUrl:'partials/people.html', controller:PeopleController}).
         when('/prisms/publications', {templateUrl:'partials/publications.html', controller:PublicationsController}).
         otherwise({redirectTo:'/prisms'});
