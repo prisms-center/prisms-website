376073a4ebcc5e50313fe0a9ad976570aca1a911
tammasr <tammasr@umich.edu>
2015-04-02 17:04:30 -0400

    changed to the ui-router and updates to the about page partials

--- assets/js/prisms/prisms-app.js
+++ assets/js/prisms/prisms-app.js
@@ -1,15 +1,65 @@
 'use strict';
 
-var app = angular.module('prisms', []);
+var app = angular.module('prisms', ['ui.router']);
 
-app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-    $routeProvider.
-        when('/prisms', {templateUrl:'partials/front-page.html', controller:FrontPageController}).
-        when('/prisms/about', {templateUrl:'partials/about.html', controller:AboutController}).
-        when('/prisms/people', {templateUrl:'partials/people.html', controller:PeopleController}).
-        when('/prisms/publications', {templateUrl:'partials/publications.html', controller:PublicationsController}).
-        when('/prisms/contact', {templateUrl:'partials/contact.html', controller:ContactsController}).
+app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
 
-        otherwise({redirectTo:'/prisms'});
-}]);
+    $stateProvider
+        .state('prisms', {
+            url: '/prisms',
+            templateUrl: 'partials/front-page.html',
+            controller: 'FrontPageController'
+        })
+        .state('about', {
+            url: '/about',
+            templateUrl: 'partials/about.html'
+        })
+        .state('people', {
+            url: '/people',
+            templateUrl: 'partials/people.html'
+        })
+        .state('publications', {
+            url: '/publications',
+            templateUrl: 'partials/publications.html'
+        })
+        .state('contact', {
+            url: '/contact/123',
+            templateUrl: 'partials/contact.html'
+        })
+        .state('about.ctools', {
+            url: '/computationaltools',
+            templateUrl: 'partials/computational_tools.html'
+        })
+        .state('about.mcommons', {
+            url: '/materialscommons',
+            templateUrl: 'partials/materials_commons.html'
+        })
+        .state('about.science', {
+            url: '/science',
+            templateUrl: 'partials/science.html'
+        })
+        .state('about.collaborators', {
+            url: '/collaborators',
+            templateUrl: 'partials/collaborators.html'
+        });
+    $urlRouterProvider.otherwise('/prisms');
+}])
 
+    .controller("NavController", function ($scope, $location, $state) {
+        $scope.isActive = function (viewLocation) {
+            return viewLocation === $location.path();
+        };
+
+        $scope.goTo = function (tab) {
+            $state.go(tab)
+        };
+
+    })
+
+    .controller("FrontPageController", function ($scope) {
+        $scope.date = new Date();
+        $scope.viewCircle = function (what) {
+            console.log('yes');
+            $scope.info = what;
+        };
+    });
